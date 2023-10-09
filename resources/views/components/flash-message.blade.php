a3325ffb94cc35c8bdce21df528f45e72a511e0d
lonewanderer27 <adrianejames03@gmail.com>
2023-10-10 00:00:25 +0800

    fix: flash message

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -4,6 +4,6 @@
     x-init="setTimeout(() => {  show = false  }, 3000)" 
     x-show="show"
     class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-laravel text-white px-48 py-3 top-0">
-    <p>{{session['message']}}</p>
+    <p>{{session('message')}}</p>
   </div>
 @endif
